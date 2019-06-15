90a03a8d6aeaa20a394a370053db721a610b73ed
severovlink <severovlink@gmail.com>
2019-06-15 17:29:50 +0300

    Add models and templates

--- flask-app/start.py
+++ flask-app/start.py
@@ -1,9 +1,30 @@
-from flask import Flask
+from collections import namedtuple
+
+from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
+Message = namedtuple('Message', 'text tag')
+messages = []
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+
+@app.route('/main', methods=['GET'])
+def main():
+    return render_template('main.html', messages=messages)
+
+
+@app.route('/add_message', methods=['POST'])
+def add_message():
+    text = request.form['text']     # запрос на получение данных из указанной в декораторе формы
+    tag = request.form['tag']
+
+    messages.append(Message(text, tag))     # передача данных в кортеж
+
+    return redirect(url_for('main'))
